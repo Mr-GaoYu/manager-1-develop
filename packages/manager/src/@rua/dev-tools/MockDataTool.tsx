301e130be8c8b9cf6c42d599badf0e8dea31e968
Mr_GaoYu <Mr_GaoYu@163.com>
2021-01-24 19:41:29 +0800

    mock

--- packages/manager/src/@rua/dev-tools/MockDataTool.tsx
+++ packages/manager/src/@rua/dev-tools/MockDataTool.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import Grid from 'src/components/core/Grid';
 
 const MockDataTool: React.FC = () => {
-  return <Grid></Grid>;
+  return (
+    <Grid>
+      <Grid item xs={12}>
+        <h4 style={{ marginBottom: 8 }}>Mock Data</h4>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default React.memo(MockDataTool);
