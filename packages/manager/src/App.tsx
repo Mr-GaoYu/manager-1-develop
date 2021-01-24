e1bf07180d3cdd4f01f9cac0d105ca588fd8ecb3
Mr_GaoYu <Mr_GaoYu@163.com>
2021-01-24 19:27:31 +0800

    husky

--- packages/manager/src/App.tsx
+++ packages/manager/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">  asdasdas                 </div>;
+  return <div className="App"> asdasdas </div>;
 }
 
 export default App;
