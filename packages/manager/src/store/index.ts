d1b2df3db7d4c17a4213f6a1ea15cbece6a0bce6
Mr_GaoYu <Mr_GaoYu@163.com>
2021-01-25 17:04:57 +0800

    store

---
+++ packages/manager/src/store/index.ts
@@ -0,0 +1,29 @@
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
+
+/**
+ * Default State
+ */
+const __resourcesDefaultState = {};
+
+export interface ResourcesState {}
+
+export interface ApplicationState {
+  __resources: ResourcesState;
+}
+
+export const defaultState: ApplicationState = {
+  __resources: __resourcesDefaultState
+};
+
+/**
+ * Reducers
+ */
+const __resources = combineReducers({});
+
+const reducers = combineReducers<ApplicationState>({
+  __resources
+});
+
+const enhancers = compose(applyMiddleware());
+
+export default createStore(reducers, defaultState, enhancers);
