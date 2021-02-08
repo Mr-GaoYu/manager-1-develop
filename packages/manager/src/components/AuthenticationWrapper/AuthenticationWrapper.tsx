4409285c6608b7a788fa3c5076c287c86a58909f
Mr_GaoYu <Mr_GaoYu@163.com>
2021-02-08 15:12:22 +0800

    AuthenticationWrapper

--- packages/manager/src/components/AuthenticationWrapper/AuthenticationWrapper.tsx
+++ packages/manager/src/components/AuthenticationWrapper/AuthenticationWrapper.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const AuthenticationWrapper: React.FC<{}> = () => {
-  return <div>2</div>;
+const AuthenticationWrapper: React.FC<{}> = (props) => {
+  const [showChildren] = React.useState<boolean>(false);
+
+  return (
+    <React.Fragment>{showChildren ? props.children : null}</React.Fragment>
+  );
 };
+
+AuthenticationWrapper.defaultProps = {
+  isAuthenticated: false
+};
+AuthenticationWrapper.displayName = 'AuthenticationWrapper';
 
 export default AuthenticationWrapper;
